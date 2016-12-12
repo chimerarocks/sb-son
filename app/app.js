50c9394dba8dfd60c5efcc7370ca6f8cbbda6e8c
João Pedro <joaopedrodslv@gmail.com>
2016-12-12 18:46:16 -0200

    chat com multiplos usuarios

--- app/app.js
+++ app/app.js
@@ -74,15 +74,10 @@ var chatComponent = Vue.extend({
     created: function() {
         var roomRef = 'chat/rooms/' + this.$route.params.room
         this.$bindAsArray('messages', db.ref(roomRef + '/messages'))
-        this.$set('user.photo', 'http://www.gravatar.com/avatar/' + md5(this.user.email) + '.jpg')
     },
     data: function() {
         return {
-            user: {
-                email: 'eu@gmail.com',
-                name: 'Eu',
-                photo: ''
-            },
+            user: JSON.parse(window.localStorage.getItem('user')),
             message: ''
         }
     },
@@ -112,7 +107,31 @@ var roomsComponent = Vue.extend({
                 <div class="panel-body">
                     {{ o.description }}
                     <br/>
-                    <a href="javascript:void(0)" @click="goToChat(o)">Entrar</a>
+                    <a href="javascript:void(0)" @click="openModal(o)">Entrar</a>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="modalLoginEmail" tabindex="-1" role="dialog" aria-labelledby="modalLoginEmail">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="exampleModalLabel">Entre com as informações</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="email" v-model="email" placeholder="E-mail">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="name" v-model="name" placeholder="Nome">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-primary" @click="login">Login</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -120,12 +139,27 @@ var roomsComponent = Vue.extend({
     firebase: {
         rooms: db.ref('chat/rooms')
     },
-    created: function() {
-        console.log(rooms[0])
+    data: function() {
+        return {
+            name: '',
+            email: '',
+            room: null
+        }
     },
     methods: {
-        goToChat: function(room) {
-            this.$route.router.go('/chat/' + room._id)
+        login: function() {
+            var user = {
+                name: this.name,
+                email: this.email,
+                photo: 'http://www.gravatar.com/avatar/' + md5(this.email) + '.jpg'
+            }
+            window.localStorage.setItem('user', JSON.stringify(user))
+            $('#modalLoginEmail').modal('hide')
+            this.$route.router.go('/chat/' + this.room._id)
+        },
+        openModal: function(room) {
+            this.room = room
+            $('#modalLoginEmail').modal('show')
         }
     }
 })
