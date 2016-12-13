f062681a06321763647c16f5556ebf3bf2a1d8e0
João Pedro <joaopedrodslv@gmail.com>
2016-12-12 23:30:22 -0200

    integrando Babel com webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,17 @@ module.exports = {
             $: 'jquery',
             jQuery: 'jquery'
         })
-    ]
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
 }
