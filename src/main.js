e48efa716efda463627f56e37b886a930ec5a232
João Pedro <joaopedrodslv@gmail.com>
2016-12-13 03:56:45 -0200

    criando AppComponent

--- src/main.js
+++ src/main.js
@@ -5,48 +5,103 @@ import _ from 'lodash'
 require('style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css')
 require('bootstrap')
 
-new Vue({
-    el: '#app',
-    data: {
-        order: {
-            keys: ['pontos', 'gm', 'gs'],
-            sort: ['desc', 'desc', 'asc']
-        },
-        colunas: ['nome', 'pontos', 'gm', 'gs', 'saldo'],
-        novoJogo: {
-            casa: {
-                time: null,
-                gols: 0
+let appComponent = Vue.extend({
+    template: `
+    <div class="container">
+        <div class="row">
+            <h3>Campeonato Braileiro - Série A - 2016</h3>
+            <a class="btn btn-primary" @click.prevent="criaNovoJogo">Novo Jogo</a>
+            <br/><br/>
+            <div v-if="view == 'tabela'">
+                <input type="text" class="form-control" v-model="filter">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th v-for="o in colunas">
+                                <a href="#" @click.prevent="sortBy(o)">{{ o | ucwords}}</a>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="o in teamsOrdered">
+                        <td>
+                            <img :src="o.escudo" style="heigth: 30px; width: 30px">
+                            <strong>{{o.nome}}</strong>
+                        </td>
+                        <td>{{o.pontos}}</td>
+                        <td>{{o.gm}}</td>
+                        <td>{{o.gs}}</td>
+                        <td>{{o | saldo}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!--<div v-if="view == 'novo jogo'">-->
+            <div v-else>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <input type="text" class="form-control" v-model="novoJogo.casa.gols" @keyup.enter="fimJogo">
+                        <label class="control-label">
+                            {{novoJogo.casa.time.nome}}
+                            <img :src="novoJogo.casa.time.escudo" style="height: 30px; width: 30px;">
+                        </label>
+                    </div>
+                    <span>X</span>
+                    <div class="form-group">
+                        <label class="control-label">
+                            <img :src="novoJogo.fora.time.escudo" style="height: 30px; width: 30px;">
+                            {{novoJogo.fora.time.nome}}
+                        </label>
+                        <input type="text" class="form-control" v-model="novoJogo.fora.gols" @keyup.enter="fimJogo">
+                    </div>
+                    <button type="button" class="btn btn-primary" @click="fimJogo">Fim de jogo</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    `,
+    data(){
+        return {
+            order: {
+                keys: ['pontos', 'gm', 'gs'],
+                sort: ['desc', 'desc', 'asc']
             },
-            fora: {
-                time: null,
-                gols: 0
-            }
-        },
-        teams: [
-            new Team('Palmeiras', require('./assets/palmeiras_60x60.png')),
-            new Team('Flamengo', require('./assets/flamengo_60x60.png')),
-            new Team('Atlético-MG', require('./assets/atletico_mg_60x60.png')),
-            new Team('Santos', require('./assets/santos_60x60.png')),
-            new Team('Botafogo', require('./assets/botafogo_60x60.png')),
-            new Team('Atlético-PR', require('./assets/atletico-pr_60x60.png')),
-            new Team('Corinthians', require('./assets/corinthians_60x60.png')),
-            new Team('Grêmio', require('./assets/gremio_60x60.png')),
-            new Team('Fluminense', require('./assets/fluminense_60x60.png')),
-            new Team('Ponte Preta', require('./assets/ponte_preta_60x60.png')),
-            new Team('Chapecoense', require('./assets/chapecoense_60x60.png')),
-            new Team('São Paulo', require('./assets/sao_paulo_60x60.png')),
-            new Team('Cruzeiro', require('./assets/cruzeiro_60x60.png')),
-            new Team('Sport', require('./assets/sport_60x60.png')),
-            new Team('Curitiba', require('./assets/coritiba_60x60.png')),
-            new Team('Internacional', require('./assets/internacional_60x60.png')),
-            new Team('Vitória', require('./assets/vitoria_60x60.png')),
-            new Team('Figueirense', require('./assets/figueirense_60x60.png')),
-            new Team('Santa Cruz', require('./assets/santa_cruz_60x60.png')),
-            new Team('América-MG', require('./assets/america_mg_60x60.png'))
-        ],
-        view: 'tabela',
-        filter: ''
+            colunas: ['nome', 'pontos', 'gm', 'gs', 'saldo'],
+            novoJogo: {
+                casa: {
+                    time: null,
+                    gols: 0
+                },
+                fora: {
+                    time: null,
+                    gols: 0
+                }
+            },
+            teams: [
+                new Team('Palmeiras', require('./assets/palmeiras_60x60.png')),
+                new Team('Flamengo', require('./assets/flamengo_60x60.png')),
+                new Team('Atlético-MG', require('./assets/atletico_mg_60x60.png')),
+                new Team('Santos', require('./assets/santos_60x60.png')),
+                new Team('Botafogo', require('./assets/botafogo_60x60.png')),
+                new Team('Atlético-PR', require('./assets/atletico-pr_60x60.png')),
+                new Team('Corinthians', require('./assets/corinthians_60x60.png')),
+                new Team('Grêmio', require('./assets/gremio_60x60.png')),
+                new Team('Fluminense', require('./assets/fluminense_60x60.png')),
+                new Team('Ponte Preta', require('./assets/ponte_preta_60x60.png')),
+                new Team('Chapecoense', require('./assets/chapecoense_60x60.png')),
+                new Team('São Paulo', require('./assets/sao_paulo_60x60.png')),
+                new Team('Cruzeiro', require('./assets/cruzeiro_60x60.png')),
+                new Team('Sport', require('./assets/sport_60x60.png')),
+                new Team('Curitiba', require('./assets/coritiba_60x60.png')),
+                new Team('Internacional', require('./assets/internacional_60x60.png')),
+                new Team('Vitória', require('./assets/vitoria_60x60.png')),
+                new Team('Figueirense', require('./assets/figueirense_60x60.png')),
+                new Team('Santa Cruz', require('./assets/santa_cruz_60x60.png')),
+                new Team('América-MG', require('./assets/america_mg_60x60.png'))
+            ],
+            view: 'tabela',
+            filter: ''
+        }
     },
     methods: {
         fimJogo() {
@@ -95,5 +150,12 @@ new Vue({
         ucwords(word) {
             return word.charAt(0).toUpperCase() + word.slice(1)
         }
+    }
+})
+
+new Vue({
+    el: '#app',
+    components: {
+        'app': appComponent
     }
 })
