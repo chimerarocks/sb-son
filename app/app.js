697723825b9e9bef84fb2e3e7dae044da2617950
João Pedro <joaopedrodslv@gmail.com>
2016-12-12 23:32:26 -0200

    ES6 - primeiras importacoes

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 require([
-    'vue', 'vue-router', './rooms-component', './rooms-create-component', './chat-component'
-], function(Vue, VueRouter, RoomsComponent, RoomsCreateComponent, ChatComponent) {
+    './rooms-component', './rooms-create-component', './chat-component'
+], function(RoomsComponent, RoomsCreateComponent, ChatComponent) {
     Vue.use(VueRouter)
 
     var appComponent = Vue.extend({})
