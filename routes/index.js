9d8f87a4c113e798234525d046f7610d3ac9acb0
chimerarocks <joaopedrodslv@gmail.com>
2017-01-17 11:36:59 -0200

    montando nosso crawler

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request')
+var cheerio = require('cheerio')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var options = {
+  	url: 'http://www.themovieblog.com/category/features/reviews/',
+  	method: 'GET'
+  }
+
+  request(options, function(err, response, body) {
+  	if (err || response.statusCode != 200) {
+  		return;
+  	}
+
+  	var $ = cheerio.load(body);
+
+  	res.end($('.genaral-post-item').toString())
+  })
 });
 
 module.exports = router;
