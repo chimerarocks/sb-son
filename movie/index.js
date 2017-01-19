8cd0fe821a1e33035121520f360ea7fa96e26cb6
chimerarocks <joaopedrodslv@gmail.com>
2017-01-19 10:16:25 -0200

    capturando dados

--- movie/index.js
+++ movie/index.js
@@ -1,5 +1,6 @@
 const Hapi = require('hapi');
 const mongoose = require('mongoose');
+const Horseman = require('node-horseman');
 
 const server = new Hapi.Server();
 server.connection({
@@ -7,6 +8,7 @@ server.connection({
 });
 
 const db = mongoose.connect('mongodb://localhost/crawler').connection;
+const horseman = new Horseman();
 
 db.on('error', (err) => {
 	console.log(`Mongoose Error = ${err}`)
@@ -14,9 +16,49 @@ db.on('error', (err) => {
 
 server.route({
 	method: 'GET',
-	path: '/',
+	path: '/movies',
 	handler: (req,replay) => {
-		replay('Hello from Hapi.js')
+		horseman
+			.open('http://www.listchallenges.com/disney-movies')
+            .evaluate(function() {
+                $ = window.$ || window.jQuery;
+
+                var movies = [];
+                var skeleton = {
+                    name: '',
+                    year: ''
+                };
+
+                $('.item-name').each(function(index, el) {
+                    var name = $(el).text();
+                    var year = name.match(/\(([^)]+)\)/);
+
+                    if (!year) {
+                        return;
+                    }
+
+                    year = year[1];
+                    name = name.replace(/\s*\(.*?\)\s*/g, '');
+
+                    skeleton.name = name;
+                    skeleton.year = year;
+
+                    movies.push(skeleton);
+                    skeleton = {
+                        name: '',
+                        year: ''
+                    };
+                });
+
+                return movies;
+            })
+			.then(function(res) {
+				replay(res)
+			})
+			.catch(function(err) {
+				console.log(`There is an error: ${err}`)
+			})
+			.close();
 	}
 })
 
