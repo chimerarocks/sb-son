8a0b5aa298bc734a4df6263550194afd1a0eecf9
João Pedro <joaopedrodslv@gmail.com>
2017-01-20 23:12:24 -0200

    eventos

--- handle.js
+++ handle.js
@@ -1,7 +1,21 @@
+var querystring = require('querystring');
+
 function handle(request, response) {
   response.writeHead(200, {
     'Content-Type': 'text/html'
   });
+
+  var body = '';
+
+  request.on('data', function (data) {
+    body += data.toString();
+  });
+
+  request.on('end', function () {
+    var decode = querystring.parse(body);
+    console.log(decode)
+  });
+
   response.write('<!DOCTYPE html>');
   response.write('<html>');
   response.write('<head>');
@@ -16,4 +30,4 @@ function handle(request, response) {
 
 // o Node só executará o module.exports porque tem prioridade, visto que exports é um alias
 module.exports = handle;
-exports.fn = handle
+exports.fn = handle;
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var http = require('http');
-var handle = require('./handle')
+var handle = require('./handle');
+
+var events = require('events')
+var emitter = new events.EventEmitter();
+
+// emitter.on('say', say);
+//
+// function say() {
+//   console.log('im say');
+// }
+//
+// emitter.emit('say');
 
 var server = http.createServer(handle);
 
