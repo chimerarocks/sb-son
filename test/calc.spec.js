8ecebe753d5462e9b359692a972d8218d853cc4e
João Pedro <joaopedrodslv@gmail.com>
2017-01-21 02:49:17 -0200

    sinon

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -1,10 +1,33 @@
-var calc = require('./../calc');
-var expect = require('chai').expect;
+const calc = require('./../calc');
+const expect = require('chai').expect;
+const sinon = require('sinon');
 
-describe('calc test', function() {
-  it ('should sum return 4', function(done) {
-    var result = calc.sum(2,2);
-    expect(result).to.equal(4);
-    done();
+describe('calc test', () => {
+  it('should sum return 4', (done) => {
+    let mock = sinon.mock(calc);
+
+    mock.expects('sum').yields(null, 4);
+
+    calc.sum(2,2, (err, result) => {
+      mock.verify();
+      mock.restore();
+      expect(result).to.equal(4);
+      done();
+    });
+    // let result = calc.sum(2,2);
+    // expect(result).to.equal(4);
+    // done();
   });
+  it('stub func', (done) => {
+    let stub = sinon.stub(calc, 'sum');
+    stub.returns(5);
+
+    var result = calc.calc(4,1);
+
+    expect(calc.sum).to.have.been.calledOnce;
+
+    expect(result).to.equal(5);
+    stub.restore();
+    done();
+  })
 });
