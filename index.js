d5eee2486b1381a6e4a53f15a1f953cfb28aaac7
João Pedro <joaopedrodslv@gmail.com>
2017-01-22 02:34:56 -0200

    cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http');
 var bodyParser = require('body-parser')
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+// var cors = require('cors');
 
 var app = express();
 var server     = http.createServer(app);
@@ -11,6 +12,30 @@ var server     = http.createServer(app);
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extend: true }));
 app.use(bodyParser.json());
+
+//cors
+// var whitelist = [
+//   'http://google.com',
+//   'http://expressjs.com'
+// ];
+// app.use(cors({
+//   origin: function(origin, cb) {
+//     var index = whitelist.index(origin);
+//     if (index !== -1) {
+//       cb(null, index);
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT']
+// }));
+
+// app.use(function(req,res,next) {
+//   res.header('Access-Control-Allow-Origin', 'http://expressjs.com');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+// });
+// app.use(function(req,res,next) {
+//   res.header('Access-Control-Allow-Origin', 'http://expressjs.com');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+// });
 
 //database
 var mongoDb = mongoose.connect('mongodb://localhost/ecommerce').connection;
