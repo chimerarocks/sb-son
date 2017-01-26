72c51cb09ac6a1624ce90d261ce70e8a93b81eac
chimerarocks <joaopedrodslv@gmail.com>
2017-01-26 11:27:14 -0200

    configurando banco de dados

---
+++ data/schema.sql
@@ -0,0 +1,10 @@
+create table post (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL,
+	content TEXT NOT NULL
+);
+
+insert into post(title, content) values ('Post 1', 'Content 1');
+insert into post(title, content) values ('Post 2', 'Content 2');
+insert into post(title, content) values ('Post 3', 'Content 3');
+insert into post(title, content) values ('Post 4', 'Content 4');		
