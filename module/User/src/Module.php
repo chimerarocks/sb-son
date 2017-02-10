4a144e57e4cf46e9dd7d06fbd0fc9a659f3b12c5
chimerarocks <joaopedrodslv@gmail.com>
2017-02-10 17:43:15 -0200

    Criando proteção da area administrativa

--- module/User/src/Module.php
+++ module/User/src/Module.php
@@ -10,9 +10,29 @@ use Zend\Authentication\AuthenticationServiceInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ControllerProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\Mvc\MvcEvent;
 
 class Module implements ConfigProviderInterface, ServiceProviderInterface, ControllerProviderInterface
 {
+	public function onBootstrap(MvcEvent $e)
+	{
+		$eventManager = $e->getApplication()->getEventManager();
+		$container = $e->getApplication()->getServiceManager();
+
+		$eventManager->attach(MvcEvent::EVENT_DISPATCH, function (MvcEvent $e) use ($container) {
+			$match = $e->getRouteMatch();
+
+			$authService = $container->get(AuthenticationServiceInterface::class);
+			$routeName = $match->getMatchedRouteName();
+			if ($authService->hasIdentity()) {
+				return;
+			} elseif (strpos($routeName, 'admin') !== false) {
+				$match->setParam('controller', AuthController::class)
+					->setParam('action', 'login');
+			}
+		}, 100);
+	}
+
 	public function getConfig()
 	{
 		return include __DIR__ . '/../config/module.config.php';
