e6545993cd02996804bc61f946a5525eece0b56c
chimerarocks <joaopedrodslv@gmail.com>
2017-02-10 14:21:55 -0200

    Criando tabela de usuários

--- data/schema.sql
+++ data/schema.sql
@@ -8,3 +8,13 @@ insert into post(title, content) values ('Post 1', 'Content 1');
 insert into post(title, content) values ('Post 2', 'Content 2');
 insert into post(title, content) values ('Post 3', 'Content 3');
 insert into post(title, content) values ('Post 4', 'Content 4');		
+
+
+create table users (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	username VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(60),
+	full_name VARCHAR(100) NOT NULL
+);
+
+insert into users(username, password, full_name) values ('joao@user.com', '$2y$10$kYAcvnIlpW0FcN36Q6svCOexCp6c6faeQjng2/.HLtQbs/Tz2qYEC', 'João Pedro');
