d46a563fcf9874d47fac63cabe4035d82e3b63b4
chimerarocks <joaopedrodslv@gmail.com>
2017-04-04 15:21:04 -0300

    view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Lumen</title>
+	</head>
+	<body>
+		
+		<h1>Bem vindo {{$name}}</h1>	
+
+		<h3>Recursos disponíveis</h3>
+
+		<ul>
+			<li>
+				<a href="/api/users">/api/users</a>
+			</li>
+		</ul>
+	
+	</body>
+</html>
