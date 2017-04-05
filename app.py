3aa3b21f8b44633ef70fa84c4c57f68077f8b50c
chimerarocks <joaopedrodslv@gmail.com>
2017-04-05 15:46:49 -0300

    oo

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from chimerarocks.math.calc import add, sub
 from chimerarocks.string.str import sum_string
 from car import Car
+from bank import *
 
 add(2,2)
 sub(3,1)
 
 sum_string("acabou", "agora")
 
-gol = Car()
+gol = Car('Gol', 'Volks', '2015')
 print(gol.name)
+print(Car.doors)
+gol.drive()
+Car.hello()
+
+Car.show()
+
+
+print('--------------------------')
+
+joao = Bank2Account(123546,5000)
+
+print(joao.get_total())
+
+joao.deposit(100)
+
+print(joao.get_total())
+
+joao.withdraw(50)
+
+print(joao.get_total())
--- car.py
+++ car.py
@@ -1,6 +1,25 @@
 class Car:
-	def __init__(self):
-		self.name = 'gol'
-		self.maker = 'wolks'
-		self.color = 'silver'
-		self.year = '2017'
+
+	doors = 4
+
+	def __init__(self, name, maker, year):
+		self.name = name
+		self.maker = maker
+		self.year = year
+
+	def drive(self):
+		print(self.name + ' started')
+
+	'''
+	Método statico é independente de instância
+	'''
+	@staticmethod
+	def hello():
+		print('hello car')
+
+	'''
+	Possui acesso aos atributos estáticos da classe, mas não da instância
+	'''
+	@classmethod
+	def show(cls):
+		print(cls.doors)
