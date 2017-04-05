8215a5fdcf73df7504ed3938095b58f0bdcd7ba4
chimerarocks <joaopedrodslv@gmail.com>
2017-04-05 15:08:29 -0300

    criando package

---
+++ app.py
@@ -0,0 +1,8 @@
+from chimerarocks.math.calc import add, sub
+from chimerarocks.string.str import sum_string
+
+
+add(2,2)
+sub(3,1)
+
+sum_string("acabou", "agora")
---
+++ chimerarocks/string/str.py
@@ -0,0 +1,2 @@
+def sum_string(str1, str2):
+	print(str1 + " " + str2)
