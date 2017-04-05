0d3e8584811f2cfc45c49abac5a31d57beb81a92
chimerarocks <joaopedrodslv@gmail.com>
2017-04-05 11:14:54 -0300

    function

---
+++ function.py
@@ -0,0 +1,12 @@
+def call_numbers():
+    for number in range(0,10):
+        print(number)
+
+call_numbers()
+
+def call_numbers_with_limit(limit):
+    list = range(0,10)
+    for number in list[0:limit]:
+        print(number)
+
+call_numbers_with_limit(3)
