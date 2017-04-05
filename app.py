4f49f1f93c633249a8fd15d10c69f610979aa838
chimerarocks <joaopedrodslv@gmail.com>
2017-04-05 15:13:08 -0300

    criando nossa classe

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from chimerarocks.math.calc import add, sub
 from chimerarocks.string.str import sum_string
-
+from car import Car
 
 add(2,2)
 sub(3,1)
 
 sum_string("acabou", "agora")
+
+gol = Car()
+print(gol.name)
---
+++ car.py
@@ -0,0 +1,6 @@
+class Car:
+	def __init__(self):
+		self.name = 'gol'
+		self.maker = 'wolks'
+		self.color = 'silver'
+		self.year = '2017'
