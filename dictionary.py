5ddb97161cd800e2de87f19f01e9f05deceba991
chimerarocks <joaopedrodslv@gmail.com>
2017-04-05 11:24:51 -0300

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+cars = {}
+
+cars['corola'] = "red"
+cars['fiat'] = "green"
+cars['320'] = "black"
+
+print(cars)
+
+print(cars.keys())
+
+print(cars.values())
+
+d = dict(Car='fiat', House='ny')
+
+family = {
+    'father' : 'Joao'
+}
+
+print(d)
+print(family)
