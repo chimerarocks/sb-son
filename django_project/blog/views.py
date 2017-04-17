e663fc0fe84fd5c4340069b9b293a54200ceee53
chimerarocks <joaopedrodslv@gmail.com>
2017-04-17 14:59:10 -0300

    Iniciando com banco de dado

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -4,13 +4,18 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Category
+
 def home(request):
 
 	name = 'João'
 
 	context = {
 		'name': name,
-		'languages': ['php', 'js', 'ruby', 'python']
+		'languages': ['php', 'js', 'ruby', 'python'],
+		'categories': Category.objects.all()
 	}
+
+	# Category.objects.create(name="Phyton")
 
 	return render(request, 'blog/home.html', context)
