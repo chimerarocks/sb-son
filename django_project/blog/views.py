e7cddb76e489e033ce195c1ce4e4633d207a9ebd
chimerarocks <joaopedrodslv@gmail.com>
2017-04-17 15:24:16 -0300

    Criando primeiro post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -4,18 +4,27 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from blog.models import Category
+from blog.models import Category, Post
 
 def home(request):
 
 	name = 'João'
 
+	category_python = Category.objects.get(pk=1)
+
+	# post = Post()
+	# post.name = 'My first very Post'
+	# post.content = 'Content of my first Post'
+	# post.status = 'Published'
+	# post.category = category_python
+	# post.save()
+
 	context = {
 		'name': name,
 		'languages': ['php', 'js', 'ruby', 'python'],
-		'categories': Category.objects.all()
+		'categories': Category.objects.all(),
+		'post': Post.objects.get(pk=1)
 	}
 
-	# Category.objects.create(name="Phyton")
 
 	return render(request, 'blog/home.html', context)
